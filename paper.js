cf832afbf25935f5dcc81597491b7e169f6a5463
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-10-24 21:16:13 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,37 @@
+class Paper {
+
+    constructor(x,y,radius){
+        this.body=Bodies.circle(x,y,radius,{isStatic:true});
+        this.r = radius;
+        World.add(world,this.body);
+        
+
+
+
+
+    }
+
+    display(){
+        
+        var pos = this.body.position;
+        ellipseMode(RADIUS);
+        fill("red");
+        ellipse(pos.x,pos.y,this.r);
+            
+
+
+
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
